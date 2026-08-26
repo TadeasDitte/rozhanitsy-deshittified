557af011c554fa8966bfc09ce7b3073deef839ab
TadeasDitte <tadeas.ditte@gmail.com>
2026-08-26 17:30:33 +0200

    feat: scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('nvd:ingest')
+    ->dailyAt('02:00')
+    ->withoutOverlapping()
+    ->onOneServer()
+    ->runInBackground();
+
+Schedule::command('osv:ingest')
+    ->dailyAt('02:00')
+    ->withoutOverlapping()
+    ->onOneServer()
+    ->runInBackground();
