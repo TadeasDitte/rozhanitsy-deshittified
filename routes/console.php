f82c95243ef2f4fbd4ac3e15f19af29a728aae5a
TadeasDitte <tadeas.ditte@gmail.com>
2026-09-10 19:20:13 +0200

    bug: fixed console.php, removed unused model

--- routes/console.php
+++ routes/console.php
@@ -1,26 +1,33 @@
 <?php
 
-use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('nvd:ingest')
+Schedule::command('ingest:nvd')
     ->dailyAt('02:00')
     ->withoutOverlapping()
     ->onOneServer()
     ->runInBackground();
 
-Schedule::command('osv:ingest')
+Schedule::command('ingest:osv')
     ->dailyAt('02:00')
     ->withoutOverlapping()
     ->onOneServer()
     ->runInBackground();
+
 Schedule::command('parse:l1')
     ->dailyAt('03:00')
+    ->withoutOverlapping()
+    ->onOneServer()
+    ->runInBackground();
+
+Schedule::command('parse:l2')
+    ->dailyAt('03:30')
     ->withoutOverlapping()
     ->onOneServer()
     ->runInBackground();
